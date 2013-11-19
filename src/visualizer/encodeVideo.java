0967b9a706d6a8d0156436f8f7204e1938b35422
ihernandez22 <isaac.hho@gmail.com>
2013-11-18 22:05:31 -0500

    Adding JPEG Video
    Adding JPEG recording and Video Encoding... to test on stand alone app

--- src/visualizer/encodeVideo.java
+++ src/visualizer/encodeVideo.java
@@ -14,11 +14,11 @@ public class encodeVideo {
 	public encodeVideo(int frames, double vidRate, long sTime, long[] timeStamps){
 		IMediaWriter writer;
 		
-		writer = ToolFactory.makeWriter("processingSketch.h264");
+		writer = ToolFactory.makeWriter("processingSketch.mov");
 		writer.open();
 		writer.setForceInterleave(true);
-		writer.addVideoStream(0,0,IRational.make((double)vidRate),800,400);
-		BufferedImage bgr = new BufferedImage(800,400,BufferedImage.TYPE_3BYTE_BGR);
+		writer.addVideoStream(0,0,IRational.make((double)vidRate),1080,720);
+		BufferedImage bgr = new BufferedImage(1080,720,BufferedImage.TYPE_3BYTE_BGR);
 		  
 		for(int i = 0; i<frames; i++){
 		     Image img = Toolkit.getDefaultToolkit().getImage("pics/img"+i+".jpg");
